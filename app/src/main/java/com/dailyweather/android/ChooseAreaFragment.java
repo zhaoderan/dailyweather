91190de8bade6e4670b48b31e7ecad84edc8a3d3
zhaoderan <zhaoderan@126.com>
2020-07-02 16:58:32 +0800

    下班前的备份

---
+++ app/src/main/java/com/dailyweather/android/ChooseAreaFragment.java
@@ -0,0 +1,27 @@
+package com.dailyweather.android;
+
+import android.app.ProgressDialog;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE=0;
+    public static final int LEVEL_CITY=1;
+    public static final int LEVEL_COUNTY=2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList =new ArrayList<>();
+    /**
+     * 生列表
+     */
+}
